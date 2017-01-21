6ed381a651a757eee853d5f32097004f7a918004
aishwat <aishwat.singh@gmail.com>
2017-01-21 17:03:24 +0530

    template

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express')
 var app = express()
 
+app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.send('Save the date 23rd feb')
 })
 
+
 app.listen(80, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Express app listening on port 80')
 })
